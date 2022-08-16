44dbbd2561e0be4d163d47b0e2a7505bab983553
Dan <dxkim201@gmail.com>
2022-08-16 16:20:43 -0400

    Fully Working Version!

--- java.js
+++ java.js
@@ -1,50 +1,70 @@
-//Create 16x16 grid of square divs
+//global selectors
 const pixelScreen = document.querySelector('.pixelScreen');
-
-let pixelAmount = 16;
-
-
-//Set screen size
-pixelScreen.style.width = ((50*pixelAmount) + "px")
-pixelScreen.style.Height = ((50*pixelAmount) + "px")
-pixelScreen.style.maxWidth = ((50*pixelAmount) + "px")
-pixelScreen.style.maxHeight = ((50*pixelAmount) + "px")
-
-//Creates pixels
-for (let i = 0; i < pixelAmount*pixelAmount; i++){
-    const pixel = document.createElement('div');
-    pixel.setAttribute('id',i.toString());
-    pixel.classList.add('pixel');
-
-    //Applies border radius to corner pixels
-    if (i === 0){
-        pixel.classList.add('topLeftPixel')
-    }
-    else if (i === pixelAmount - 1){
-        pixel.classList.add('topRightPixel')
-    }
-    else if (i === ((pixelAmount * (pixelAmount-1)))){
-        pixel.classList.add('botLeftPixel')
-    }
-    else if (i === (pixelAmount * pixelAmount)-1){
-        pixel.classList.add('botRightPixel')
-    }
-
-    //When user overs mouse over a pixel div, adds a hover class which changes bg-color to black
-    pixel.addEventListener('mouseover', function(e){
-        pixel.classList.add('hover')
-    })
-
-    //Send to screen!
-    pixelScreen.appendChild(pixel)
-}
-
-//Reset Button Functionality
 const resetBtn = document.querySelector('.resetBtn');
+const sizeBtn = document.querySelector('.sizeBtn')
 const pixels = document.querySelectorAll('.pixel');
 
+//Create default canvas
+let pixelAmount = 32;
+setCanvasSize(pixelAmount);
+createCanvas(pixelAmount);
+
+//Set screen size
+function setCanvasSize(canvasSize){
+    const pixelScreen = document.querySelector('.pixelScreen');
+    pixelScreen.style.maxWidth = ((20*canvasSize) + "px");
+    pixelScreen.style.maxHeight = ((20*canvasSize) + "px");
+}
+
+//Creates canvas based on how much pixels is inputted
+function createCanvas(canvasSize){
+    for (let i = 0; i < canvasSize*canvasSize; i++){
+        const pixel = document.createElement('div');
+        pixel.setAttribute('id',i.toString());
+        pixel.classList.add('pixel');
+    
+        //Applies border radius to corner pixels
+        if (i === 0){
+            pixel.classList.add('topLeftPixel')
+        }
+        else if (i === canvasSize - 1){
+            pixel.classList.add('topRightPixel')
+        }
+        else if (i === ((canvasSize * (canvasSize-1)))){
+            pixel.classList.add('botLeftPixel')
+        }
+        else if (i === (canvasSize * canvasSize)-1){
+            pixel.classList.add('botRightPixel')
+        }
+
+        pixel.addEventListener('mouseover', function(e){
+            pixel.classList.add('hover')
+        })
+        //Send to screen!
+        pixelScreen.appendChild(pixel)
+    }
+}
+
+//When user overs mouse over a pixel div, adds a hover class which changes bg-color to black
+
+//Reset Button Functionality
 resetBtn.addEventListener('click', function(e){
+    const pixels = document.querySelectorAll('.pixel');
     pixels.forEach(pixel => {
         pixel.classList.remove('hover');
     })
 });
+
+//Canvas Size Button Functionality
+//delete grid
+sizeBtn.addEventListener('click', function(e){
+    const pixels = document.querySelectorAll('.pixel');
+    pixelAmount = prompt('Please enter a number for canvas width and height: ')
+    pixels.forEach(pixel => {
+        pixel.remove();
+    })
+    setCanvasSize(pixelAmount);
+    createCanvas(pixelAmount);
+})
+//prompt user for new size
+//create new grid
